c0357ba09a5a0c16d721650dfca6406099a57b6a
davideschiera <davide.schiera@gmail.com>
2018-05-02 16:32:00 -0700

    Fix upload

--- build/upload-s3.sh
+++ build/upload-s3.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+set -x
+
 VERSION=$1
 BUILD_ID=$2
+BRANCH_NAME=$3
 
 FILE_NAME_PREFIX="grafana-sysdig-datasource"
 BUILD_FILE_NAME="${FILE_NAME_PREFIX}-v${VERSION}.${BUILD_ID}"
 LATEST_FILE_NAME="${FILE_NAME_PREFIX}-v${VERSION}"
 S3_BUCKET="s3://download.draios.com"
 DIST_PATH="dist"
+
+if [[ "$BRANCH_NAME" == 'master' ]]; then
+    S3_DEST="stable/grafana-sysdig-datasource"
+elif [[ "$BRANCH_NAME" == 'dev' ]]; then
+    S3_DEST="dev/grafana-sysdig-datasource"
+else
+    S3_DEST="dev/grafana-sysdig-datasource/${BRANCH_NAME}"
+fi
 
 echo "Cleaning up artifacts...."
 cp -R ${DIST_PATH} sysdig
